cb6209e3aa1a28964fd58f646ae6c93c7dc6848e
Patrick Keenan <sirberus.io@gmail.com>
2019-01-08 23:18:32 -0800

    add base64url-encoded LNK

--- src/hooks/check-if-link-exists.js
+++ src/hooks/check-if-link-exists.js
@@ -1,9 +1,26 @@
-// Use this hook to manipulate incoming or outgoing data.
-// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+// Hook checks if an appropriate record already exists; 
+// If so, it returns that instead of proceeding.
+// Otherwise, it generates the LNK.
+const base64url = require('base64url');
 
 // eslint-disable-next-line no-unused-vars
 module.exports = function (options = {}) {
   return async context => {
-    return context;
+    await context.service
+      .find({ query: { url: context.data.url } })
+      .then(async d => {
+        if (d.data.length) {
+          let record = d.data[0]
+          record.message = "LNK for this URL already existed"
+          context.result = record
+        } else {
+          await context.service
+            .find({ query: { $limit: 0 } })
+            .then(header => {
+              context.data.LNK = base64url(`${header.total}`)
+            })
+        }
+      })
+      .catch(d => console.log('error', d))
   };
 };
